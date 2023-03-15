7024b554e0ace95fae4cdfd2e2c17ce4aa69ef5a
Aliaksandra Savelyeva <aliaksandra.savelyeva@kyndryl.com>
2023-03-15 16:56:17 +0100

    locators3

---
+++ src/selenium_course_intro1/Locators3.java
@@ -0,0 +1,21 @@
+package selenium_course_intro1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Locators3 {
+
+	public static void main(String[] args) {
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--remote-allow-origins=*");
+		WebDriver driver = new ChromeDriver(options);
+		
+		// siblings - parent traverse
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		System.out.println(driver.findElement(By.xpath("//header/div/button[1]/following-sibling::button[1]")).getText());
+
+	}
+
+}
