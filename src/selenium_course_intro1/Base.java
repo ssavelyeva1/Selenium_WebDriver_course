98a1a66b1ae09c6ffd0c3ac404ce58d354d132e1
ssavelyeva1 <aliaksandra.savelyeva@gmail.com>
2023-03-20 18:13:51 +0100

    adding items to cart refactored

--- src/selenium_course_intro1/Base.java
+++ src/selenium_course_intro1/Base.java
@@ -1,5 +1,6 @@
 package selenium_course_intro1;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -10,25 +11,40 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Base {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--remote-allow-origins=*");
 		WebDriver driver = new ChromeDriver(options);
 
-		String[] names = {"Cucumber", "Broccoli"};
+		String[] itemsNeeded = {"Cucumber","Brocolli","Beetroot"};
 		
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
+		Thread.sleep(3000);
+		addItems(driver,itemsNeeded);
+		
+	}
+	
+	public static void addItems(WebDriver driver, String[] itemsNeeded) {
 
+		int j = 0;
 		List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));
 
 		for (int i = 0; i < products.size(); i++) {
-			String productName = products.get(i).getText();
+			// formatting array elements to match product list
+			String[] productName = products.get(i).getText().split("-");
+			String formattedName = productName[0].trim();
 			
-			if(productName.contains("Cucumber")) {
-				// click on Add to cart
-				driver.findElements(By.xpath("//button[text()='ADD TO CART']")).get(i).click();
-				break;
+			// converting array to array list for easy search
+			List<String> itemsNeededList = Arrays.asList(itemsNeeded);
+			
+			// checking whether the extracted name is present in the array list
+			if(itemsNeededList.contains(formattedName)) {
+				j++;
+				driver.findElements(By.xpath("//div[@class='product-action']/button")).get(i).click();
+				if(j == itemsNeeded.length) {
+					break;
+				}
 			}
 		}
 
