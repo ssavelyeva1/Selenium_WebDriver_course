cc2e83dac392ce110214e938187858bc24775347
ssavelyeva1 <aliaksandra.savelyeva@gmail.com>
2023-03-21 13:20:51 +0100

    implicit and explicit wait

--- src/selenium_course_intro1/Base.java
+++ src/selenium_course_intro1/Base.java
@@ -1,30 +1,51 @@
 package selenium_course_intro1;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Base {
 
+	@SuppressWarnings("deprecation")
 	public static void main(String[] args) throws InterruptedException {
 
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--remote-allow-origins=*");
 		WebDriver driver = new ChromeDriver(options);
-
-		String[] itemsNeeded = {"Cucumber","Brocolli","Beetroot"};
 		
+		// implicit and explicit waits declaration
+		// driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		WebDriverWait w = new WebDriverWait(driver, Duration.ofSeconds(5));
+		
+		String[] itemsNeeded = { "Cucumber", "Brocolli", "Beetroot" };
+
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
 		Thread.sleep(3000);
-		addItems(driver,itemsNeeded);
+		addItems(driver, itemsNeeded);
+
+		driver.findElement(By.cssSelector("img[alt='Cart']")).click();
+		driver.findElement(By.xpath("//button[contains(text(),'PROCEED TO CHECKOUT')]")).click();
+		
+		w.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input.promoCode")));
+		driver.findElement(By.cssSelector("input.promoCode")).sendKeys("rahulshettyacademy");
+		driver.findElement(By.cssSelector("button.promoBtn")).click();
+		
+		w.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span.promoInfo")));
+		
+		System.out.println(driver.findElement(By.cssSelector("span.promoInfo")).getText());
 		
 	}
-	
+
+	// method for automating the process of adding items in the cart
 	public static void addItems(WebDriver driver, String[] itemsNeeded) {
 
 		int j = 0;
@@ -34,15 +55,15 @@ public class Base {
 			// formatting array elements to match product list
 			String[] productName = products.get(i).getText().split("-");
 			String formattedName = productName[0].trim();
-			
+
 			// converting array to array list for easy search
 			List<String> itemsNeededList = Arrays.asList(itemsNeeded);
-			
+
 			// checking whether the extracted name is present in the array list
-			if(itemsNeededList.contains(formattedName)) {
+			if (itemsNeededList.contains(formattedName)) {
 				j++;
 				driver.findElements(By.xpath("//div[@class='product-action']/button")).get(i).click();
-				if(j == itemsNeeded.length) {
+				if (j == itemsNeeded.length) {
 					break;
 				}
 			}
