5f189e8fd52f95ab292d6542782225cb7dd105ec
ssavelyeva1 <aliaksandra.savelyeva@gmail.com>
2023-03-21 14:17:17 +0100

    fluent wait

--- src/selenium_course_intro1/Base.java
+++ src/selenium_course_intro1/Base.java
@@ -3,8 +3,6 @@ package selenium_course_intro1;
 import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -15,17 +13,16 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Base {
 
-	@SuppressWarnings("deprecation")
 	public static void main(String[] args) throws InterruptedException {
 
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--remote-allow-origins=*");
 		WebDriver driver = new ChromeDriver(options);
-		
+
 		// implicit and explicit waits declaration
 		// driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		WebDriverWait w = new WebDriverWait(driver, Duration.ofSeconds(5));
-		
+
 		String[] itemsNeeded = { "Cucumber", "Brocolli", "Beetroot" };
 
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
@@ -34,15 +31,15 @@ public class Base {
 
 		driver.findElement(By.cssSelector("img[alt='Cart']")).click();
 		driver.findElement(By.xpath("//button[contains(text(),'PROCEED TO CHECKOUT')]")).click();
-		
+
 		w.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input.promoCode")));
 		driver.findElement(By.cssSelector("input.promoCode")).sendKeys("rahulshettyacademy");
 		driver.findElement(By.cssSelector("button.promoBtn")).click();
-		
+
 		w.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span.promoInfo")));
-		
+
 		System.out.println(driver.findElement(By.cssSelector("span.promoInfo")).getText());
-		
+
 	}
 
 	// method for automating the process of adding items in the cart
---
+++ src/selenium_course_intro1/fluentWaitTest.java
@@ -0,0 +1,43 @@
+package selenium_course_intro1;
+
+import java.time.Duration;
+import java.util.function.Function;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+
+public class fluentWaitTest {
+
+	public static void main(String[] args) {
+
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--remote-allow-origins=*");
+		WebDriver driver = new ChromeDriver(options);
+
+		driver.get("https://the-internet.herokuapp.com/dynamic_loading/1");
+		driver.findElement(By.cssSelector("[id='start'] button")).click();
+
+		Wait<WebDriver> wait = new FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds(30))
+				.pollingEvery(Duration.ofSeconds(3)).ignoring(NoSuchElementException.class);
+
+		WebElement foo = (WebElement) wait.until(new Function<WebDriver, WebElement>() {
+			public WebElement apply(WebDriver driver) {
+				if (driver.findElement(By.cssSelector("[id='finish'] h4")).isDisplayed()) {
+					return driver.findElement(By.cssSelector("[id='finish'] h4"));
+				} else {
+					return null;
+				}
+			}
+		});
+
+		System.out.println(driver.findElement(By.cssSelector("[id='finish'] h4")).getText());
+
+	}
+
+}
