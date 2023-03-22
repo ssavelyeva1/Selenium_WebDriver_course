2a06d0528ead21724cdf0afaf2cd132259fb3e3f
ssavelyeva1 <aliaksandra.savelyeva@gmail.com>
2023-03-22 17:09:48 +0100

    soft asserts

--- src/selenium_course_intro1/BrokenLinks.java
+++ src/selenium_course_intro1/BrokenLinks.java
@@ -4,11 +4,15 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.asserts.SoftAssert;
 
 public class BrokenLinks {
 
@@ -20,15 +24,31 @@ public class BrokenLinks {
 
 		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
 
-		String url = driver.findElement(By.cssSelector("a[href*='brokenlink']")).getAttribute("href");
+		List<WebElement> links = driver.findElements(By.cssSelector("li[class='gf-li'] a"));
 
-		// calling URL and getting the status code
-		HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
-		conn.setRequestMethod("HEAD");
-		conn.connect();
-		int responseCode = conn.getResponseCode();
-		System.out.println(responseCode);
-		
+		SoftAssert a = new SoftAssert();
+
+		for (WebElement link : links) {
+			String url = link.getAttribute("href");
+
+			// calling URL and getting the status code
+			HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+			conn.setRequestMethod("HEAD");
+			conn.connect();
+			int responseCode = conn.getResponseCode();
+			System.out.println(responseCode);
+
+			// Assert.assertTrue(responseCode < 400, "The link with text " + link.getText()
+			// + " is broken with code " + responseCode); - hard assert
+
+			// soft assert (will not stop test execution
+			a.assertTrue(responseCode < 400,
+					"The link with text " + link.getText() + " is broken with code " + responseCode);
+
+		}
+
+		a.assertAll();
+
 	}
 
 }
