ff1412ded7fbf701d3ca83e258c371e5cd226a71
ssavelyeva1 <aliaksandra.savelyeva@gmail.com>
2023-03-22 16:12:31 +0100

    driver options

--- src/selenium_course_intro1/SSLCheck.java
+++ src/selenium_course_intro1/SSLCheck.java
@@ -1,5 +1,9 @@
 package selenium_course_intro1;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.openqa.selenium.Proxy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -10,7 +14,17 @@ public class SSLCheck {
 
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--remote-allow-origins=*");
+		// handling insecure certificates
 		options.setAcceptInsecureCerts(true);
+		// setting proxy
+		Proxy proxy = new Proxy();
+		proxy.setHttpProxy("ipaddress:4444");
+		options.setCapability("proxy", proxy);
+		
+		// setting download preferences
+		Map<String, Object> prefs = new HashMap<String, Object>();
+		prefs.put("sownload.default_directory", "/directory/path");
+		options.setExperimentalOption("prefs", prefs);
 		
 		WebDriver driver = new ChromeDriver(options);
 		
