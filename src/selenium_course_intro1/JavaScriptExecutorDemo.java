c498a982a6844f69a55361b5e1b7855598a36902
ssavelyeva1 <aliaksandra.savelyeva@gmail.com>
2023-03-22 15:09:25 +0100

    js executor upd

--- src/selenium_course_intro1/JavaScriptExecutorDemo.java
+++ src/selenium_course_intro1/JavaScriptExecutorDemo.java
@@ -1,9 +1,14 @@
 package selenium_course_intro1;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
 
 public class JavaScriptExecutorDemo {
 
@@ -22,6 +27,18 @@ public class JavaScriptExecutorDemo {
 		Thread.sleep(3000);
 		js.executeScript("document.querySelector('.tableFixHead').scrollTop=5000");
 		
+		List<WebElement> values = driver.findElements(By.cssSelector(".tableFixHead td:nth-child(4)"));
+		
+		int sum = 0;
+		for(int i = 0; i < values.size(); i++) {
+			sum = sum + Integer.parseInt(values.get(i).getText());
+		}
+		
+		System.out.println(sum);
+		
+		int total = Integer.parseInt(driver.findElement(By.cssSelector(".totalAmount")).getText().split(":")[1].trim());
+		
+		Assert.assertEquals(sum, total);
 		
 	}
 
