d0dd131c85afbdc1e73707920feadb378c80a420
ssavelyeva1 <aliaksandra.savelyeva@gmail.com>
2023-04-03 12:08:01 +0200

    extracting data from table using streams

--- src/selenium_course_intro1/WebTableSorting.java
+++ src/selenium_course_intro1/WebTableSorting.java
@@ -17,24 +17,35 @@ public class WebTableSorting {
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--remote-allow-origins=*");
 		WebDriver driver = new ChromeDriver(options);
-		
+
 		driver.get("https://www.rahulshettyacademy.com/seleniumPractise/#/offers");
-		
+
 		// clicking on the column
 		driver.findElement(By.xpath("//tr/th[1]")).click();
-		
+
 		// capturing all web elements into the list
 		List<WebElement> elementsList = driver.findElements(By.xpath("//tr/td[1]"));
-		
+
 		// capturing text of all elements into new list using streams
-		List<String> originalList = elementsList.stream().map(s->s.getText()).collect(Collectors.toList());
-		
+		List<String> originalList = elementsList.stream().map(s -> s.getText()).collect(Collectors.toList());
+
 		// sorting the original list
 		List<String> sortedList = originalList.stream().sorted().collect(Collectors.toList());
-		
+
 		// comparing two lists
 		Assert.assertTrue(originalList.equals(sortedList));
 
+		// scanning the name column and getting the price
+		List<String> price = elementsList.stream().filter(s -> s.getText().contains("Beans"))
+				.map(s -> getVeggiePrice(s)).collect(Collectors.toList());
+		
+		price.forEach(a->System.out.println(a));
+
+	}
+
+	private static String getVeggiePrice(WebElement s) {
+		String priceValue = s.findElement(By.xpath("following-sibling::td[1]")).getText();
+		return priceValue;
 	}
 
 }
