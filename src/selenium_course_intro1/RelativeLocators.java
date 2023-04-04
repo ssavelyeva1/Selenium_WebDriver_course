98c2dac6eb0458ad55ef510f351d5aef3fe82f12
ssavelyeva1 <aliaksandra.savelyeva@gmail.com>
2023-04-04 10:55:23 +0200

    relative locators

---
+++ src/selenium_course_intro1/RelativeLocators.java
@@ -0,0 +1,26 @@
+package selenium_course_intro1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.openqa.selenium.support.locators.RelativeLocator.*;
+
+public class RelativeLocators {
+
+	public static void main(String[] args) {
+
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--remote-allow-origins=*");
+		WebDriver driver = new ChromeDriver(options);
+
+		driver.get("https://rahulshettyacademy.com/angularpractice/");
+
+		WebElement nameEditBox = driver.findElement(By.cssSelector("[name='name']"));
+		System.out.println(driver.findElement(with(By.tagName("label")).above(nameEditBox)).getText());
+	
+	}
+
+}
