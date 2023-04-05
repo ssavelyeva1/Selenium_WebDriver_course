6a7083b89ad02ee85bfad72272fc9cbfca962eac
ssavelyeva1 <aliaksandra.savelyeva@gmail.com>
2023-04-05 13:11:21 +0200

    screenprint of webelement

--- src/selenium_course_intro1/NewWindow.java
+++ src/selenium_course_intro1/NewWindow.java
@@ -1,17 +1,22 @@
 package selenium_course_intro1;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.Set;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class NewWindow {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--remote-allow-origins=*");
@@ -26,15 +31,20 @@ public class NewWindow {
 		String parentWindowId = it.next();
 		String childWindowId = it.next();
 
-		// switching control to the newly open window
+		// switching control to the newly open tab
 		driver.switchTo().window(childWindowId);
 		driver.get("https://rahulshettyacademy.com/");
 
+		// getting the course name and entering it to the Name field
 		String courseName = driver.findElements(By.cssSelector("a[href*='https://courses.rahulshettyacademy.com/p']"))
 				.get(1).getText();
 		driver.switchTo().window(parentWindowId);
-		driver.findElement(By.cssSelector("[name='name']")).sendKeys(courseName);
-		driver.quit();
+		WebElement fieldName = driver.findElement(By.cssSelector("[name='name']"));
+		fieldName.sendKeys(courseName);
+		
+		// capturing a screenprint of a webelement and saving it to the project folder
+		File file = fieldName.getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(file, new File("logo.png"));
 
 	}
 
